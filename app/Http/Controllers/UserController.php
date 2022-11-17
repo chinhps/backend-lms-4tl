651343fa8a50407b4ffe24bbcd092d9d9c3fc15f
tienbotay <phamlenhattien1997@gmail.com>
2022-11-17 22:40:38 +0700

    m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends BaseController
 {
-   public function list(Request $reuqest,$id)
+   public function list()
    {
-        $data_input = $reuqest->all();
-        // $data = DB::table('users')->paginate(3);
-        $data = DB::table('users')->updateOrInsert([
-            'id' => $id
-        ],[
-            'name' => "asdasd"
-        ]);
-        return response()->json($data,404);
+      $data = DB::table('users')->join('classes', 'users.class_id', '=', 'classes.id')->join('role', 'users.role_id', '=', 'role.id')
+      ->select('users.id', 'users.user_code', 'users.email','users.phone_number','users.name','users.status', 'users.role_id', 'users.class_id', 'users.created_at','users.updated_at','classes.class_name', 'role.role_name')->get();
+      return response()->json($data);
    }
 }
