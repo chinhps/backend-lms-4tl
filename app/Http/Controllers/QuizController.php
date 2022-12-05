4299748dca541fbb98d0c773d819ef01c010128a
unknown <phamchinh347@gmail.com>
2022-12-05 23:38:16 +0700

    add new

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -12,13 +12,26 @@ use Illuminate\Support\Facades\DB;
 
 class QuizController extends Controller
 {
-    public function getQuizCourse(Request $request)
-    {
-        $slug_course = $request->input('slug_course');
-        $slug_quiz = $request->input('slug_quiz');
-        $course = Course::where('slug',$slug_course)->first();
 
-        $list_question = QuestionBank::where('subject_id',$course->subject_id)->take(10)->get();
+    public function joinQuiz(Request $request)
+    {
+        $password = $request->input('password');
+        $slug_quiz = $request->input('slug_quiz');
+        $slug_course = $request->input('slug_course');
+
+        $quiz = Quiz::with('deadlines')->where('slug', $slug_quiz)->first();
+        if ($quiz->password != null && $password != $quiz->password) {
+            return BaseResponse::ResWithStatus("Mật khẩu sai không thể làm bài!", 403);
+        }
+
+        return $quiz;
+        
+        $course = Course::where('slug', $slug_course)->first();
+        $list_question = QuestionBank::where('subject_id', $course->subject_id)->take(10)->get();
+
+        // if() {
+
+        // }
 
         return  QuestionResource::collection($list_question);
     }
