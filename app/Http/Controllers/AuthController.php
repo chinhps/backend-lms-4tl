9be3b04c37fe1445f25de2376f0e4edaac3e4d8a
chinhdev <phamchinh347@gmail.com>
2022-12-09 01:32:45 +0700

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\UserLogin;
 use App\Http\Requests\UserRegister;
 use App\Models\User;
 use App\Repositories\User\UserInterface;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -17,7 +18,7 @@ class AuthController extends Controller
     {
         $this->userRepository = $userRepository;
     }
-    
+
     public function Login(UserLogin $request)
     {
         $validated = $request->validated();
@@ -42,16 +43,34 @@ class AuthController extends Controller
         return response()->json(['user' => $user, 'msg' => 'Tao tai khoan thanh cong!']);
     }
 
-    public function getMe() {
+    public function getMe()
+    {
         $data_user = Auth::user()->load(['role']);
         return BaseResponse::ResWithStatus($data_user);
     }
-    
-    public function logout() {
-        Auth::logout();
-         return response()->json(['msg' => 'Dang xuat thành công!']);
+
+    public function change_password(Request $request)
+    {
+        $password_old = $request->input('password_old');
+        $password = $request->input('password');
+        // $password = $request->input('password_confirmation');
+
+        $userData = User::find(Auth::id());
+        if ($userData->password == null || Hash::check($password_old, $userData->password)) {
+            $userData->password = Hash::make($password);
+            $userData->save();
+            return BaseResponse::ResWithStatus("Đổi mật khẩu thành công!");
+        } else {
+            return BaseResponse::ResWithStatus("Mật khẩu cũ bạn nhập chưa đúng!",403);
+        }
     }
-        
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(['msg' => 'Dang xuat thành công!']);
+    }
+
     public function ListUsers()
     {
         $data = $this->userRepository->getList();
